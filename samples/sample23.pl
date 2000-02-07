badfe73f53b12e0fc90891674248358c31023548
mgjv <mgjv>
2000-02-07 09:56:12 +0000

    Added sample23

--- samples/sample23.pl
+++ samples/sample23.pl
@@ -1,29 +1,101 @@
-# NOT ACTIVE IN DISTRIBUTION, REQUIRES OVERWRITE = 2 TO WORK
 use GD::Graph::area;
 require 'save.pl';
 
+# CONTRIB Edwin Hildebrand.
+#
+# See changes in bars.pm: Check for bar height rounding errors when 
+# stacking bars.
+
 print STDERR "Processing sample 2-3\n";
 
-@data = ( 
-    ["1st","2nd","3rd","4th","5th","6th","7th", "8th", "9th"],
-    [    5,   12,   24,   33,   19,    8,    6,    15,    21],
-    [    1,    2,    5,    6,    3,  1.5,    1,     3,     4]
+@dat = qw(
+	991006 991007 991114 991117 991118 991119 991120 
+	991121 991122 991123 991124 991125 991126 991127 
+	991128 991129 991130 991201 991204 991205 991206 
+	991207 991208
 );
 
-$my_graph = new GD::Graph::area();
+@sub = qw(0 0 0 0 0 0 0 0 1 1 1 1 2 3 1 1 1 1 2 2 6 8 8);
+@def = qw(0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0);
+@rej = qw(0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0);
+@opn = qw(4 4 4 5 4 4 4 4 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3);
+@ass = qw(0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0);
+@wrk = qw(1 2 2 2 2 2 1 1 2 2 2 1 1 1 1 1 1 1 1 1 3 6 5);
+@fin = qw(0 0 0 0 0 0 1 0 0 0 0 1 1 1 2 2 2 2 2 2 2 2 2);
+@ver = qw(0 0 0 0 0 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1);
+@con = qw(0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0);
+@tst = qw(0 0 0 0 0 0 0 0 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1);
+@rev = qw(0 0 0 0 0 0 0 0 1 1 2 1 1 1 1 1 1 1 1 1 1 1 1);
+@cco = qw(0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 1 0 0 0 0 0 0);
+@cls = qw(0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 1 1 0 0 0 0);
+@sld = qw(0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 2 2 3 3 3 4);
 
-$my_graph->set( 
-	x_label => 'X Label',
-	y_label => 'Y label',
-	title => 'An Weird Area Graph',
-	y_max_value => 40,
-	y_tick_number => 8,
-	y_label_skip => 2,
-	dclrs => ['white', 'blue'],
-	borderclrs => ['white', 'black'],
+# setup x data
+push(@data,\@dat);         # push x labels into plot data
+push(@data,\@sub);
+push(@data,\@def);
+push(@data,\@rej);
+push(@data,\@opn);
+push(@data,\@ass);         # push x values into plot data
+push(@data,\@wrk);         # (push order must match legend label order)
+push(@data,\@fin);
+push(@data,\@ver);
+push(@data,\@con);
+push(@data,\@tst);
+push(@data,\@rev);
+push(@data,\@cco);
+push(@data,\@cls);
+push(@data,\@sld);
+
+# setup legend labels
+@legend = qw(
+	Submitted Deferred Rejected Opened Assigned Work
+	Finished Verified Configured Tested Reviewed
+	Closed-CO Closed Sealed
 );
 
-$my_graph->set_legend( 'empty', 'data' );
-$my_graph->plot(\@data);
-save_chart($my_graph, 'sample23');
+# get graph object
+$graph = GD::Graph::area->new(640, 480);
+
+# set graph legend
+$graph->set_legend(@legend);
+
+# set graph options
+$graph->set(
+   'dclrs'            => [ qw(lblue lyellow blue yellow lgreen lred
+						      green red purple orange pink dyellow) ],
+   'title'            => "States by Time",
+   'x_label'          => "Time",
+   'y_label'          => "# OF thingies",
+   'long_ticks'       => 1,
+   'tick_length'      => 0,
+   'x_ticks'          => 0,
+   'x_label_position' => '.5',     # centered x label
+   'y_label_position' => '.5',     # centered y label
+
+#   This should have overwrite = 2
+#   'overwrite'        => 2,        # stacked x data
+
+   'bgclr'            => 'white',  # makes background transparent
+   'transparent'      => 0,
+   'interlaced'       => 1,        # show like venetian blind opens
+
+								   # number of ticks on y axis
+   'y_tick_number'    => 5,
+   'y_number_format'  => '%d',     # integer y tick values
+								   # min & max adjusted to make tick
+   'y_max_value'      => 10,
+   'y_min_value'      => 0,
+
+   'y_plot_values'    => 1,        # display tick values
+   'x_plot_values'    => 1,        # display tick values
+   'x_labels_vertical'=> 1,        # display tick values vertically
+   'zero_axis'        => 1,        # show line at y value =0
+   'lg_cols'          => 7,        # num legend columns
+
+   'accent_treshold'  => 55,
+);
+
+$graph->plot(\@data);
+save_chart($graph, 'sample23');
 

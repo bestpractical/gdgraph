36bf786efb8434e24332fe70938e819db2989f25
mgjv <mgjv>
2000-02-13 06:13:24 +0000

    Moved overwrite == 2 into main draw_data_set

--- Graph/bars.pm
+++ Graph/bars.pm
@@ -5,7 +5,7 @@
 #	Name:
 #		GD::Graph::bars.pm
 #
-# $Id: bars.pm,v 1.9 2000/02/13 03:55:43 mgjv Exp $
+# $Id: bars.pm,v 1.10 2000/02/13 06:13:24 mgjv Exp $
 #
 #==========================================================================
  
@@ -19,49 +19,48 @@ use GD::Graph::colour qw(:colours);
 
 @GD::Graph::bars::ISA = qw(GD::Graph::axestype);
 
-sub draw_data
+sub draw_data_old
 {
-	my $s = shift;
-	my $d = shift;
+	my $self = shift;
 
-	if ( $s->{overwrite} ) 
+	if ( $self->{overwrite} ) 
 	{
-		$s->draw_data_overwrite($d);
+		$self->draw_data_overwrite();
 	} 
 	else 
 	{
-		$s->SUPER::draw_data($d);
+		$self->SUPER::draw_data();
 	}
 
 	# redraw the 'zero' axis
-	$s->{graph}->line( 
-		$s->{left}, $s->{zeropoint}, 
-		$s->{right}, $s->{zeropoint}, 
-		$s->{fgci} );
+	$self->{graph}->line( 
+		$self->{left}, $self->{zeropoint}, 
+		$self->{right}, $self->{zeropoint}, 
+		$self->{fgci} );
 }
 
 # Draws the bars on top of each other
 
 sub draw_data_overwrite 
 {
-	my $s = shift;
-	my $d = shift;
-	my $g = $s->{graph};
-	my $bar_s = _round($s->{bar_spacing}/2);
+	my $self = shift;
+	my $d = $self->{_data};
+	my $g = $self->{graph};
+	my $bar_s = _round($self->{bar_spacing}/2);
 
-	my $zero = $s->{zeropoint};
+	my $zero = $self->{zeropoint};
 
 	# CONTRIB Jeremy Wadsack, shadows
-	my $bsd = $s->{shadow_depth} and
-		my $bsci = $s->set_clr(_rgb($s->{shadowclr}));
+	my $bsd = $self->{shadow_depth} and
+		my $bsci = $self->set_clr(_rgb($self->{shadowclr}));
 		
-	for my $i (0 .. $s->{_data}->num_points - 1) 
+	for my $i (0 .. $self->{_data}->num_points - 1) 
 	{
 		my $bottom = $zero;
 		my ($xp, $t);
 
 		my $total = 0;
-		for my $j (1 .. $s->{_data}->num_sets)
+		for my $j (1 .. $self->{_data}->num_sets)
 		{
 			next unless defined $d->[$j][$i];
 
@@ -71,8 +70,8 @@ sub draw_data_overwrite
 			#
 			# Instead of using rounded values to offset bars for
 			# overwrite == 2, well keep track a bit more exactly.
-			(undef, $bottom) = $s->val_to_pixel($i + 1, $total, $j)
-				if $s->{overwrite} == 2;
+			(undef, $bottom) = $self->val_to_pixel($i + 1, $total, $j)
+				if $self->{overwrite} == 2;
 			$total += $d->[$j][$i];
 
 			# get data colour
@@ -80,20 +79,20 @@ sub draw_data_overwrite
 			#
 			# cycle_clrs option sets the color based on the point, 
 			# not the dataset.
-			my $dsci = $s->set_clr($s->pick_data_clr(
-				$s->{cycle_clrs} ? $i + 1 : $j));
+			my $dsci = $self->set_clr($self->pick_data_clr(
+				$self->{cycle_clrs} ? $i + 1 : $j));
 			# contrib "Bremford, Mike" <mike.bremford@gs.com>
-			my $brci = $s->set_clr($s->pick_border_clr(
-				$s->{cycle_clrs} > 1 ? $i + 1 : $j));
+			my $brci = $self->set_clr($self->pick_border_clr(
+				$self->{cycle_clrs} > 1 ? $i + 1 : $j));
 
 			# get coordinates of top and center of bar
-			($xp, $t) = $s->{overwrite} == 2 ?
-				$s->val_to_pixel($i + 1, $total, $j) :
-				$s->val_to_pixel($i + 1, $d->[$j][$i], $j);
+			($xp, $t) = $self->{overwrite} == 2 ?
+				$self->val_to_pixel($i + 1, $total, $j) :
+				$self->val_to_pixel($i + 1, $d->[$j][$i], $j);
 			
 			# calculate left and right of bar
-			my $l = $xp - _round($s->{x_step}/2) + $bar_s;
-			my $r = $xp + _round($s->{x_step}/2) - $bar_s;
+			my $l = $xp - _round($self->{x_step}/2) + $bar_s;
+			my $r = $xp + _round($self->{x_step}/2) - $bar_s;
 
 			if ($t <= $bottom)
 			{
@@ -102,7 +101,7 @@ sub draw_data_overwrite
 					if $bsd;
 				$g->filledRectangle($l, $t, $r, $bottom, $dsci);
 				$g->rectangle($l, $t, $r, $bottom, $brci) 
-					if ($r - $l > $s->{accent_treshold});
+					if ($r - $l > $self->{accent_treshold});
 			}
 			else
 			{
@@ -111,7 +110,7 @@ sub draw_data_overwrite
 					if $bsd;
 				$g->filledRectangle($l, $bottom, $r, $t, $dsci);
 				$g->rectangle($l, $bottom, $r, $t, $brci)
-					if ($r - $l > $s->{accent_treshold});
+					if ($r - $l > $self->{accent_treshold});
 			}
 		}
 	}
@@ -119,77 +118,92 @@ sub draw_data_overwrite
 
 sub draw_data_set
 {
-	my $s = shift;
-	my $d = shift;
+	my $self = shift;
 	my $ds = shift;
-	my $g = $s->{graph};
-	my $bar_s = _round($s->{bar_spacing}/2);
+
+	my $bar_s = _round($self->{bar_spacing}/2);
 
 	# Pick a data colour
-	my $dsci = $s->set_clr($s->pick_data_clr($ds));
+	my $dsci = $self->set_clr($self->pick_data_clr($ds));
 	# contrib "Bremford, Mike" <mike.bremford@gs.com>
-	my $brci = $s->set_clr($s->pick_border_clr($ds));
+	my $brci = $self->set_clr($self->pick_border_clr($ds));
 
 	# CONTRIB Jeremy Wadsack, shadows
-	my $bsd = $s->{shadow_depth} and
-		my $bsci = $s->set_clr(_rgb($s->{shadowclr}));
+	my $bsd = $self->{shadow_depth} and
+		my $bsci = $self->set_clr(_rgb($self->{shadowclr}));
 
-	for my $i (0 .. $s->{_data}->num_points - 1) 
+	my @values = $self->{_data}->y_values($ds);
+
+	for (my $i = 0; $i < @values; $i++) 
 	{
-		next unless (defined $d->[$i]);
+		my $value = $values[$i];
+		next unless defined $value;
+
+		my $bottom = $self->{zeropoint};
+
+		if ($self->{overwrite} == 2 && $ds > 1)
+		{
+			$value = $self->{_data}->get_y_cumulative($ds, $i);
+			my $pvalue = $self->{_data}->get_y_cumulative($ds - 1, $i);
+			(undef, $bottom) = $self->val_to_pixel($i + 1, $pvalue, $ds)
+		}
 
 		# CONTRIB Jeremy Wadsack
 		#
 		# cycle_clrs option sets the color based on the point, 
 		# not the dataset.
-		$dsci = $s->set_clr($s->pick_data_clr($i + 1))
-			if $s->{cycle_clrs};
-		$brci = $s->set_clr($s->pick_data_clr($i + 1))
-			if $s->{cycle_clrs} > 1;
+		$dsci = $self->set_clr($self->pick_data_clr($i + 1))
+			if $self->{cycle_clrs};
+		$brci = $self->set_clr($self->pick_data_clr($i + 1))
+			if $self->{cycle_clrs} > 1;
 
 		# get coordinates of top and center of bar
-		my ($xp, $t) = $s->val_to_pixel($i + 1, $d->[$i], $ds);
+		my ($xp, $t) = $self->val_to_pixel($i + 1, $value, $ds);
 
 		# calculate left and right of bar
 		my ($l, $r);
 
-		if (ref $s eq 'GD::Graph::mixed')
+		if (ref $self eq 'GD::Graph::mixed' || $self->{overwrite})
 		{
-			$l = $xp - _round($s->{x_step}/2) + $bar_s;
-			$r = $xp + _round($s->{x_step}/2) - $bar_s;
+			$l = $xp - _round($self->{x_step}/2) + $bar_s;
+			$r = $xp + _round($self->{x_step}/2) - $bar_s;
 		}
 		else
 		{
 			$l = $xp 
-				- _round($s->{x_step}/2)
-				+ _round(($ds - 1) * $s->{x_step}/$s->{_data}->num_sets)
+				- _round($self->{x_step}/2)
+				+ _round(($ds - 1) * $self->{x_step}/$self->{_data}->num_sets)
 				+ $bar_s;
 			$r = $xp 
-				- _round($s->{x_step}/2)
-				+ _round($ds * $s->{x_step}/$s->{_data}->num_sets)
+				- _round($self->{x_step}/2)
+				+ _round($ds * $self->{x_step}/$self->{_data}->num_sets)
 				- $bar_s;
 		}
 
 		# draw the bar
-		if ($d->[$i] >= 0)
+		if ($value >= 0)
 		{
 			# positive value
-			$g->filledRectangle($l+$bsd, $t+$bsd, $r+$bsd, $s->{zeropoint}, 
-				$bsci) if $bsd;
-			$g->filledRectangle($l, $t, $r, $s->{zeropoint}, $dsci );
-			$g->rectangle($l, $t, $r, $s->{zeropoint}, $brci)
-				if ($r - $l > $s->{accent_treshold});
+			$self->{graph}->filledRectangle(
+				$l + $bsd, $t + $bsd, $r + $bsd, $bottom, $bsci
+			) if $bsd;
+			$self->{graph}->filledRectangle($l, $t, $r, $bottom, $dsci);
+			$self->{graph}->rectangle($l, $t, $r, $bottom, $brci) 
+				if $r - $l > $self->{accent_treshold};
 		}
 		else
 		{
 			# negative value
-			$g->filledRectangle($l+$bsd, $s->{zeropoint}, $r+$bsd, $t+$bsd, 
-				$bsci) if $bsd;
-			$g->filledRectangle($l, $s->{zeropoint}, $r, $t, $dsci);
-			$g->rectangle($l, $s->{zeropoint}, $r, $t, $brci)
-				if ($r - $l > $s->{accent_treshold});
+			$self->{graph}->filledRectangle(
+				$l + $bsd, $bottom, $r + $bsd, $t + $bsd, $bsci
+			) if $bsd;
+			$self->{graph}->filledRectangle($l, $bottom, $r, $t, $dsci);
+			$self->{graph}->rectangle($l, $bottom, $r, $t, $brci) 
+				if $r - $l > $self->{accent_treshold};
 		}
 	}
+
+	return $ds;
 }
 
 1;

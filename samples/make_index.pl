e719696e78952108082535021e09062695a1f311
mgjv <mgjv>
2003-02-25 02:41:59 +0000

    added make_index.pl, not to distro

---
+++ samples/make_index.pl
@@ -0,0 +1,54 @@
+#!/usr/local/bin/perl -w
+use strict;
+use CGI qw/:standard *table/;
+
+# Create an index file for all the samples.
+
+my $EXT = shift || "png";
+
+my %titles = (
+    sample1 => 'Bar charts',
+    sample2 => 'Area charts',
+    sample3 => 'Points charts',
+    sample4 => 'Lines and Points charts',
+    sample5 => 'Lines charts',
+    sample6 => 'Mixed charts',
+    sample7 => 'Miscellaneous things',
+    sample9 => 'Pie charts',
+);
+
+my %links;
+
+foreach my $sgroup (sort keys %titles)
+{
+    open HTML, ">$sgroup.html" or die $!;
+
+    print HTML start_html($titles{$sgroup}),
+		h1($titles{$sgroup}), start_table();
+
+    foreach my $num (1..9)
+    {
+	foreach my $img ("$sgroup$num.$EXT", "$sgroup$num-h.$EXT")
+	{
+	    if (-f $img)
+	    {
+		print HTML Tr(
+		    td(a({href => "$sgroup$num.pl"},"$sgroup$num.pl")),
+		    td(img({src => "$img"}))
+		    );
+	    }
+	}
+    }
+
+    print HTML end_table(), end_html();
+}
+
+open(HTML, ">index.html") or die $!;
+print HTML start_html('GD::Graph examples'),
+    h1('GD::Graph examples');
+foreach my $sgroup (sort keys %titles)
+{
+    print HTML p(a({href => "$sgroup.html"}, $titles{$sgroup}) );
+}
+print HTML end_html();
+
